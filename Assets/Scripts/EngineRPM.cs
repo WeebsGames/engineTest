c30e7393fdc349c3765333fe9ffd61a8800ed8cf
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-06-17 13:36:48 -0500

    added somewhat realistic gear shifting

--- Assets/Scripts/EngineRPM.cs
+++ Assets/Scripts/EngineRPM.cs
@@ -8,12 +8,16 @@ public class EngineRPM : MonoBehaviour
 
     public const int REVLIMIT = 7000;
     public const float LIMITER = 1000;
+    public int horsepower;
     public TMP_Text rpmText;
     public TMP_Text gearText;
+    public TMP_Text torqueText;
 
     float rpm = 0;
     float revs = 10;
+    float torque = 0;
     int gear = 0;
+    bool engineOn = true;
     Transform driveShaft;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,7 +31,7 @@ public class EngineRPM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && rpm < REVLIMIT)
+        if (Input.GetKey(KeyCode.Space) && rpm < REVLIMIT && engineOn)
         {
             rpm += revs;
         } else if (rpm > 0)
@@ -35,18 +39,41 @@ public class EngineRPM : MonoBehaviour
             rpm -= 1;
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && !engineOn && gear == 0)
+        {
+            engineOn = true;
+        }
+
         if (Input.GetKeyDown(KeyCode.RightArrow) && gear != 6)
         {
             gear += 1;
+            if(rpm < 3000)
+            {
+                engineOn = false;
+                rpm = 0;
+            } else
+            {
+                rpm *= 0.4f;
+            }
+
         }
         if(Input.GetKeyDown(KeyCode.LeftArrow) && gear != -1)
         {
             gear -= 1;
+            if(rpm > 5000)
+            {
+                engineOn = false;
+                rpm = 0;
+            } else
+            {
+                rpm *= 1.25f;
+            }
         }
     }
 
     void FixedUpdate()
     {
+        rpm = Mathf.Round(rpm);
         if(rpm > REVLIMIT)
         {
             rpm -= LIMITER; 
@@ -54,6 +81,7 @@ public class EngineRPM : MonoBehaviour
 
         rpmText.text = "RPM: " + rpm;
         gearText.text = "Gear: " + gear;
+        torqueText.text = "Torque: " + torque;
 
         driveShaft.Rotate(Vector3.up * rpm * Time.deltaTime);
     }
