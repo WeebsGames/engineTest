25fd8774b301734ab21e0e671ca5a0822a784c9c
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-06-17 12:50:38 -0500

    added rpm and rev limiter

--- Assets/Scripts/EngineRPM.cs
+++ Assets/Scripts/EngineRPM.cs
@@ -1,16 +1,42 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EngineRPM : MonoBehaviour
 {
+
+
+    public const int REVLIMIT = 7000;
+    public const float LIMITER = 1000;
+    public TMP_Text rpmText;
+
+    float rpm = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rpmText.text = "RPM: 0"; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) & rpm < REVLIMIT)
+        {
+            rpm += 10;
+        } else if (rpm > 0)
+        {
+            rpm -= 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(rpm > REVLIMIT)
+        {
+            rpm -= LIMITER; 
+        }
+
+        rpmText.text = "RPM: " + rpm;
     }
 }
